078cff3606196c6ed031949533d57f93659b3db0
Your Name <you@example.com>
2026-04-07 00:54:49 -0400

    Se añadio en el main una sección donde el usuario mediante una opción escoge como ejecutar el programa ya sea como consola o modo ventana.

--- Registro_Civil/src/main/java/com/registrocivil/main/Main.java
+++ Registro_Civil/src/main/java/com/registrocivil/main/Main.java
@@ -2,12 +2,36 @@ package com.registrocivil.main;
 
 import com.registrocivil.logica.GestionSistema;
 import com.registrocivil.window.VentanaMenu;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] arr){
+    public static void main(String[] arr)throws IOException{
         GestionSistema sistemaTotal = new GestionSistema();
         
-        VentanaMenu menu = new VentanaMenu(sistemaTotal);
-        menu.setVisible(true);
+        // Inicializar un lector para que el usuario escoga el modo, ya sea consola o ventana. 
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("SISTEMA DE REGISTRO CIVIL"); 
+        System.out.println("Seleccione el modo de inicio:"); 
+        System.out.println("1. Consola"); 
+        System.out.println("2. Ventana"); 
+        System.out.println("Ingrese una opción (1 o 2): ");
+        
+        try {
+            String opcion = lector.readLine();
+            if(opcion.equals("1")){
+                System.out.println("Iniciando modo consola");
+                // Se llamara una clase la cual inicializa el programa en modo consola. 
+            }
+            else if (opcion.equals("2")){
+                VentanaMenu menu = new VentanaMenu(sistemaTotal); // Se inicia el sistema en modo ventana.
+                menu.setVisible(true); 
+            }
+            else{
+                System.out.println("Opción no válida. Cerrando sistema"); 
+            }
+        } catch(IOException e){
+            System.out.println("Error al leer la entrada del usuario " + e.getMessage());
+        }
     }
 }
